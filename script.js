ccd51f58bedce57f0658dd8b1a5948b83f6cfb90
John <johnmeasey@gmail.com>
2021-05-06 21:46:48 +0200

    Add files via upload
    # try again

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-  var TITLE = 'Retractions from Life Sciences journals (1990–2020)';
+  var TITLE = 'Income and Test Scores in Connecticut School Districts, 2009-13';
 
-  var POINT_X = 'Year'; // column name for x values in data.csv
-  var POINT_X_PREFIX = 'Year'; // prefix for x values, eg 'Year'
+  var POINT_X = 'income'; // column name for x values in data.csv
+  var POINT_X_PREFIX = '$'; // prefix for x values, eg '$'
   var POINT_X_POSTFIX = ''; // postfix for x values, eg '%'
 
-  var POINT_Y = 'Normalised retractions'; // column name for y values in data.csv
-  var POINT_Y_PREFIX = 'Norm. Retractions'; // prefix for x values, eg 'USD '
+  var POINT_Y = 'grades'; // column name for y values in data.csv
+  var POINT_Y_PREFIX = ''; // prefix for x values, eg 'USD '
   var POINT_Y_POSTFIX = ''; // postfix for x values, eg ' kg'
 
-  var POINT_NAME = 'Number of Retractions'; // point names that appear in tooltip
+  var POINT_NAME = 'district'; // point names that appear in tooltip
   var POINT_COLOR = 'rgba(0,0,255,0.7)'; // eg `black` or `rgba(10,100,44,0.8)`
   var POINT_RADIUS = 5; // radius of each data point
 
-  var X_AXIS = 'Year'; // x-axis label, label in tooltip
-  var Y_AXIS = 'Normalised Retractions'; // y-axis label, label in tooltip
+  var X_AXIS = 'Median Household Income, USD'; // x-axis label, label in tooltip
+  var Y_AXIS = 'Grade, Relative to Average'; // y-axis label, label in tooltip
 
   var SHOW_GRID = true; // `true` to show the grid, `false` to hide
 
